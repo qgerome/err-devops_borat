9719e0ab352dc357faf240015fedfdc5bc262138
Quentin Gérôme <quentin.gerome@alpsquare.com>
2014-11-16 22:24:49 +0100

    Update twitter api

--- devops_borat.py
+++ devops_borat.py
@@ -1,34 +1,57 @@
 from random import choice
-from feedparser import parse
 from errbot import botcmd, BotPlugin
+import twitter
+from cache import cached_property
+
+
+consumer_key='7hQkMRDXkRcHxuVvfwYFcVh2v'
+consumer_secret='mSOagAeJQN0EqZPyhP8e1SRwDVixKGNCgd5VXeYZ6k1IGnPyyY'
+access_token_key='324841391-JT37mccobuIv185EoUf0Zl79SVxe7jW2fZpmmsk9'
+access_token_secret='0dwFpqjVAH4MEzE1iDbEd6plrUUXaYM3CxJml98O6XDvA'
+
+
+class TwitterApi(object):
+	def __init__(self, consumer_key, consumer_secret, access_token_key, access_token_secret):
+		self.api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
+		                       access_token_key=access_token_key, access_token_secret=access_token_secret)
+		super(TwitterApi, self).__init__()
+
+	@cached_property(ttl=60 * 60 * 1)
+	def get_statuses_for(self, user_name):
+		return self.api.GetUserTimeline(screen_name=user_name, count=200)
+
 
 class DevOpsBorat(BotPlugin):
-    """
+	"""
     Quotes from various dev humour related twitter accounts
     """
-    @botcmd
-    def borat(self, mess, args):
-        """
+
+	def __init__(self):
+		self.twitter = TwitterApi(consumer_key, consumer_secret, access_token_key, access_token_secret)
+		super(DevOpsBorat, self).__init__()
+
+
+	@botcmd
+	def borat(self, mess, args):
+		"""
         Random quotes from the DEVOPS_BORAT twitter account
         """
-        myfeed = parse('http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=DEVOPS_BORAT')
-        items = myfeed['entries']
-        return choice(items).description
+		values = self.twitter.get_statuses_for('DEVOPS_BORAT')
+		return choice(values).text
 
-    @botcmd
-    def jesus(self, mess, args):
-        """
+	@botcmd
+	def jesus(self, mess, args):
+		"""
         Random quotes from the devops_jesus twitter account
         """
-        myfeed = parse('http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=devops_jesus')
-        items = myfeed['entries']
-        return choice(items).description
+		values = self.twitter.get_statuses_for('devops_jesus')
+		return choice(values).text
 
-    @botcmd
-    def yoda(self, mess, args):
-        """
+	@botcmd
+	def yoda(self, mess, args):
+		"""
         Random quotes from the UXYoda twitter account
         """
-        myfeed = parse('http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=UXYoda')
-        items = myfeed['entries']
-        return choice(items).description
+		values = self.twitter.get_statuses_for('UXYoda')
+		return choice(values).text
+
